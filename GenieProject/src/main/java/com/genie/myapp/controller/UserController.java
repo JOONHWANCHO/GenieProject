15517ab44b9d5e6c2f918375d0d958082f025beb
osy3197 <107012631+osy3197@users.noreply.github.com>
2022-09-01 09:14:11 +0900

    0901-osy-genieview

--- GenieProject/src/main/java/com/genie/myapp/controller/UserController.java
+++ GenieProject/src/main/java/com/genie/myapp/controller/UserController.java
@@ -7,8 +7,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/user/*")
 public class UserController {
-	@GetMapping("genie")
-	public String genie() {
+	@GetMapping("genie_01")
+	public String genie_01() {
+		return null;
+	}
+	@GetMapping("genie_02")
+	public String genie_02() {
+		return null;
+	}
+	@GetMapping("genie_03")
+	public String genie_03() {
+		return null;
+	}
+	@GetMapping("genie_04")
+	public String genie_04() {
 		return null;
 	}
 }
