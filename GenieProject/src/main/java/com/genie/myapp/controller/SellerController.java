019a809ee7b52bce64fb44641138150deb710a51
hyuuneee <107015756+hyuuneee@users.noreply.github.com>
2022-09-02 14:37:13 +0900

    sellerForm

---
+++ GenieProject/src/main/java/com/genie/myapp/controller/SellerController.java
@@ -0,0 +1,22 @@
+package com.genie.myapp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+@RequestMapping("/seller/*")
+public class SellerController {
+	
+	ModelAndView mav = null;
+	
+	//업체 회원가입 폼 보기
+	@GetMapping("sellerForm")
+	public ModelAndView sellerForm() {
+		mav = new ModelAndView();
+		mav.setViewName("seller/sellerForm");
+		return mav;
+	}
+	
+}
