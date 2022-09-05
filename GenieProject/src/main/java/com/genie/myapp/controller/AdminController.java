f2c984186bc584fd516759809ae4f3c6bd16878e
JOONHWANCHO <joonhwanncho@gmail.com>
2022-09-05 16:30:35 +0900

    AdminMainpage

---
+++ GenieProject/src/main/java/com/genie/myapp/controller/AdminController.java
@@ -0,0 +1,26 @@
+package com.genie.myapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/admin/*")
+public class AdminController {
+		
+		@GetMapping("adminLogin")
+		public ModelAndView adminLogin() {
+			ModelAndView mav = new ModelAndView();
+			mav.setViewName("admin/adminLogin");
+			return mav;
+		}
+		
+		@GetMapping("adminMain")
+		public ModelAndView adminMain() {
+			ModelAndView mav = new ModelAndView();
+			mav.setViewName("admin/adminMain");
+			return mav;
+		}
+	
+}
