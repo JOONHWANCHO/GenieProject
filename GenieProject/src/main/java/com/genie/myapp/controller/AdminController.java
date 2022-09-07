b80e64ec5d427a0277111d4112d299422a50055b
joonhwancho <joonhwanncho@gmail.com>
2022-09-07 15:25:03 +0900

    Update Admin Files (#49)

--- GenieProject/src/main/java/com/genie/myapp/controller/AdminController.java
+++ GenieProject/src/main/java/com/genie/myapp/controller/AdminController.java
@@ -9,11 +9,65 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping("/admin/*")
 public class AdminController {
 		
+	@Autowired
+	AdminService service;
+	ModelAndView mav = null;
+	
+		@GetMapping("adminLogin")
+		public ModelAndView adminLogin() {
+			ModelAndView mav = new ModelAndView();
+			mav.setViewName("admin/adminLogin");
+			return mav;
+		}
+
 		@GetMapping("adminMain")
 		public ModelAndView adminMain() {
 			ModelAndView mav = new ModelAndView();
 			mav.setViewName("admin/adminMain");
 			return mav;
 		}
-	
+		
+		@GetMapping("adminProduct")
+		public ModelAndView adminProduct() {
+			ModelAndView mav = new ModelAndView();
+			mav.setViewName("admin/adminProduct");
+			return mav;
+		}
+
+		@GetMapping("adminPro")
+		public ModelAndView adminPro() {
+			ModelAndView mav = new ModelAndView();
+			mav.setViewName("admin/adminPro");
+			return mav;
+		}
+		
+		//@GetMapping("adminCategoryTag")
+		//public ModelAndView adminCategoryTag() {
+		//	ModelAndView mav = new ModelAndView();
+		//	mav.setViewName("admin/adminCategoryTag");
+		//	return mav;
+		//}
+		
+		//카테고리 
+		@GetMapping("adminCategoryTag")
+		public ModelAndView adminCategoryTag(AdminVO VO) {
+			mav = new ModelAndView();
+			System.out.println(VO.toString());
+			mav.addObject("list", service.adminCategoryTag(VO));
+			mav.addObject("VO", VO);
+			mav.setViewName("admin/adminCategoryTag");
+			return mav;
+		}
+
+		//태그
+		@GetMapping("adminTag")
+		public ModelAndView adminTag(AdminVO VO) {
+			mav = new ModelAndView();
+			System.out.println(VO.toString());
+			mav.addObject("list", service.adminTag(VO));
+			mav.addObject("VO", VO);
+			mav.setViewName("admin/adminTag");
+			return mav;
+		}
+		
 }
