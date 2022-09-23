3011acd650338f8847d8dab57c9d30717d7c36bb
Seho0218 <105859355+Seho0218@users.noreply.github.com>
2022-09-23 18:27:41 +0900

    Seho0218 (#192)
    * product detail
    
    * Co-authored-by: osy3197
    
    * Co-authored-by: osy3197
    
    * index url 줄이기
    
    * 비밀번호 변경(완료)
    
    * Co-authored-by: osy3197
    
    * Update genie.jsp
    
    * image
    
    * product
    
    * conflict

---
+++ GenieProject/src/main/java/com/genie/myapp/vo/TagVO.java
@@ -0,0 +1,43 @@
+package com.genie.myapp.vo;
+
+public class TagVO{
+    private String genie_who;
+    private String genie_day;
+    private String genie_old;
+    private String gender;
+
+
+    @Override
+    public String toString() {
+        return "Tag_nameVO [gender=" + gender + ", genie_day=" + genie_day + ", genie_old="
+                + genie_old + ", genie_who=" + genie_who + "]";
+    }
+    
+    public String getGenie_who() {
+        return genie_who;
+    }
+    public void setGenie_who(String genie_who) {
+        this.genie_who = genie_who;
+    }
+    public String getGenie_day() {
+        return genie_day;
+    }
+    public void setGenie_day(String genie_day) {
+        this.genie_day = genie_day;
+    }
+ 
+    public String getGenie_old() {
+        return genie_old;
+    }
+    public void setGenie_old(String genie_old) {
+        this.genie_old = genie_old;
+    }
+    public String getGender() {
+        return gender;
+    }
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+    
+    
+}
