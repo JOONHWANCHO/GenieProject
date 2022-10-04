7d609b35513d90fb8d1bf496f12ea0fbc52ec837
hanschae <jun961205@gmail.com>
2022-10-04 17:24:44 +0900

    add reply

--- GenieProject/src/main/java/com/genie/myapp/vo/ReplyProductVO.java
+++ GenieProject/src/main/java/com/genie/myapp/vo/ReplyProductVO.java
@@ -13,4 +13,44 @@ public class ReplyProductVO {
     private int product_id;
     private String comment;
     private String writedate;
+    
+    
+    
+	@Override
+	public String toString() {
+		return "ReplyProductVO [genie_id=" + genie_id + ", reply_no=" + reply_no + ", product_id=" + product_id
+				+ ", comment=" + comment + ", writedate=" + writedate + "]";
+	}
+	public String getGenie_id() {
+		return genie_id;
+	}
+	public void setGenie_id(String genie_id) {
+		this.genie_id = genie_id;
+	}
+	public int getReply_no() {
+		return reply_no;
+	}
+	public void setReply_no(int reply_no) {
+		this.reply_no = reply_no;
+	}
+	public int getProduct_id() {
+		return product_id;
+	}
+	public void setProduct_id(int product_id) {
+		this.product_id = product_id;
+	}
+	public String getComment() {
+		return comment;
+	}
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+	public String getWritedate() {
+		return writedate;
+	}
+	public void setWritedate(String writedate) {
+		this.writedate = writedate;
+	}
+    
+    
 }
