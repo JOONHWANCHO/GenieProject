66002aa762f14f63b1cfa04a4acf2be7ee9e6c3f
Seho0218 <105859355+Seho0218@users.noreply.github.com>
2022-10-10 01:16:59 +0900

    Seho0218 (#272)
    * 컨트롤러 병경(배송지 리다이렉트)
    
    * 트랜잭션 수정
    
    * 주문내역 구현
    
    * 컨트롤러 변경
    
    * 구매 페이지 (수정중)
    
    * payment 배송지 추가
    
    * 주문결제 페이지 기능 완료
    
    * 장바구니 삭제
    
    * 오전 팀별 작업
    
    * 오후작업
    
    * 셀러페이지
    
    * 결제페이지 수정중
    
    * 3시 까지 작업
    
    * 오후 작업
    
    * 장바구니 삭제
    
    * 장바구니오류, css 수정중
    
    * 주문 결제페이지(수정중)
    
    * 주문 결제페이지
    
    * 카트 오류 수정
    
    * 장바구니 수정
    
    * 오후 작업
    
    * 오후 작업(js css정리, 결제페이지 구현중 )
    
    * 스타일 옮기기 완료, 카트 체크박스
    
    * 장바구니 선택 오류 수정, 결제 수정중

--- GenieProject/src/main/webapp/js_css/js/cart_js.js
+++ GenieProject/src/main/webapp/js_css/js/cart_js.js
@@ -119,3 +119,30 @@ $(document).on('click','.cart-list input[value=Del]',function(){
             }
         });   
 });
+
+$(function(){
+    $("#multiChk").submit(function(){
+        
+        const query = 'input[name="cartList"]:checked';     
+		if($(query).length=="0"){
+			alert("1개 이상의 상품을 선택해주세요");
+			return false;
+		}  
+    })
+})
+
+
+// function getCheckedCnt()  {
+
+//     const query = 'input[id="cartList"]:checked';
+//     const selectedElements = document.querySelectorAll(query);
+
+//     document.querySelector("#multiChk").submit(function(){
+
+//     console.log("1234");
+//     if(selectedElements.length=="0"){
+//         alert("1개 이상의 상품을 선택해주세요");
+//         return false;
+//     }
+//     })
+// }
