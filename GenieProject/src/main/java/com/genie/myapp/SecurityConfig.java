4ac2912595faeae42f3ad5cf9abed1a5be6243e8
Seho0218 <ghdtpgh8913@gmail.com>
2022-10-13 19:09:01 +0900

    바로구매, 장바구니 구매

--- GenieProject/src/main/java/com/genie/myapp/SecurityConfig.java
+++ GenieProject/src/main/java/com/genie/myapp/SecurityConfig.java
@@ -22,7 +22,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable();
+		http.csrf().disable()
+		.cors().disable();
 		
 		http.authorizeRequests()
 			.antMatchers("/**").permitAll()
