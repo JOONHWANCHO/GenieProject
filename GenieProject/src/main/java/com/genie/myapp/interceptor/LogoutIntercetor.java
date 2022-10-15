8d135c85bb19938494c6cc3996daa37e33ed67f1
Seho0218 <105859355+Seho0218@users.noreply.github.com>
2022-10-15 16:13:21 +0900

    Seho0218 (#308)
    * 컨트롤러 병경(배송지 리다이렉트)
    
    * 트랜잭션 수정
    
    * 주문내역 구현
    
    * 컨트롤러 변경
    
    * 구매 페이지 (수정중)
    
    * payment 배송지 추가
    
    * 주문결제 페이지 기능 완료
    
    * 장바구니 삭제
    
    * 오전 팀별 작업
    
    * 오후작업
    
    * 셀러페이지
    
    * 결제페이지 수정중
    
    * 3시 까지 작업
    
    * 오후 작업
    
    * 장바구니 삭제
    
    * 장바구니오류, css 수정중
    
    * 주문 결제페이지(수정중)
    
    * 주문 결제페이지
    
    * 카트 오류 수정
    
    * 장바구니 수정
    
    * 오후 작업
    
    * 오후 작업(js css정리, 결제페이지 구현중 )
    
    * 스타일 옮기기 완료, 카트 체크박스
    
    * 장바구니 선택 오류 수정, 결제 수정중
    
    * 주문 결제 (거의 완료)
    
    * 스크립트 css 정리
    
    * 카트페이지 수정
    
    * 시큐리티(그래들, 폼, db등 변경)
    
    * 주문관련 컨트롤러 수정
    
    * 작업
    
    * 오후 중간작업
    
    * 시큐리티(작업중)
    
    * 변경
    
    * order 문제
    
    * 주문결제(1개만 됌)
    
    * 1개만 결제 완료
    
    * 주문 결제 완료
    
    * 오전 작업
    
    * 바로구매, 장바구니 구매
    
    * 개별결제, 장바구니 결제 완료
    
    * 변경
    
    * 코드 수정
    
    * 스프링시큐리티 작업
    
    * 시큐리티 작업 중
    
    * 시큐리티
    
    * 오더리스트 수정
    
    * css
    
    * 인터셉터
    
    * 인터셉터

---
+++ GenieProject/src/main/java/com/genie/myapp/interceptor/LogoutIntercetor.java
@@ -0,0 +1,30 @@
+package com.genie.myapp.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Component("LogoutInterceptor")
+public class LogoutIntercetor implements HandlerInterceptor {
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+		HttpSession session = request.getSession();
+		
+		String logStatus = (String)session.getAttribute("logStatus"); // 로그인 여부 확인
+		
+		if(logStatus.equals("N")) {
+
+			//로그인상태일때
+			return true;
+		}else {
+
+		//로그인 안된경우
+		response.sendRedirect("/login");
+		return false;	
+		}	
+	}
+}
---
+++ GenieProject/src/main/java/com/genie/myapp/interceptor/UserInterceptor.java
@@ -0,0 +1,28 @@
+package com.genie.myapp.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Component("UserInterceptor")
+public class UserInterceptor implements HandlerInterceptor {
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+		HttpSession session = request.getSession();
+		
+		String authority = (String)session.getAttribute("ROLE");
+		
+		if(authority.equals("ROLE_USER")) {
+			//로그인상태일때
+			return true;
+		}else {
+			//로그인 안된경우
+		response.sendRedirect("/");
+		return false;	
+		}	
+	}
+}
